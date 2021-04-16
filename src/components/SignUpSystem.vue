a3d36475bfdf50008354cac5cd17028470cf2004
Nattima Mahapan <nattima.09@mail.kmutt.ac.th>
2021-04-16 15:12:01 +0700

    62130500042 PUT Method

--- src/components/SignUpSystem.vue
+++ src/components/SignUpSystem.vue
@@ -12,7 +12,7 @@
                 type="text"
                 id="firstname"
                 v-model="firstname"
-                class="regis"
+                class="detail"
                 :class="{ 'bg-red-50': FirstnameInput }"
               />
               <p v-if="FirstnameInput" class="text-red-500">
@@ -26,7 +26,7 @@
                 type="text"
                 id="lastname"
                 v-model="lastname"
-                class="regis"
+                class="detail"
                 :class="{ 'bg-red-50': LastnameInput }"
               />
                <p v-if="LastnameInput" class="text-red-500">
@@ -39,7 +39,7 @@
                 type="text"
                 id="email"
                 v-model="email"
-                class="regis"
+                class="detail"
                 :class="{ 'bg-red-50': EmailInput }"
               />
              <p v-if="EmailInput" class="text-red-500">
@@ -52,7 +52,7 @@
                 type="text"
                 id="username"
                 v-model="username"
-                class="regis"
+                class="detail"
                 :class="{ 'bg-red-50': UsernameInput }"
               />
               <p v-if="UsernameInput" class="text-red-500">
@@ -63,9 +63,10 @@
               <label for="birth" class="text-sm text-gray-600">Birthday</label>
               <input
                 type="date"
+                name="birth"
                 id="birth"
                 v-model="birth"
-                class="regis"
+                class="detail"
                 :class="{ 'bg-red-50': BirthInput }"
               />
               <p v-if="BirthInput" class="text-red-500">
@@ -73,25 +74,47 @@
               </p>
             </div>
             <div class="format">
-              <label for="genders" class="text-sm text-gray-600">Gender</label>
+              <p class="text-sm text-gray-600">Gender</p>
+              <div class="flex flex-row justify-center py-3" >
+              
+              <div class="mr-3">
               <input
-                id="genders"
+                type="radio"
+                name="genders"
+                id="genders-male"
+                value="male"
                 v-model="genders"
-                class="regis"
-                :class="{ 'bg-red-50': GenderInput }"
                 />
-              <p v-if="GenderInput" class="text-red-500">
-                Please enter your gender!
+              <label for="genders-male" class="ml-2 text-sm text-gray-600">Male</label>
+              </div>
+
+              <div class="ml-3">
+              <input
+                type="radio"
+                name="genders"
+                id="genders-female"
+                value="female"
+                v-model="genders"
+                />
+                <label for="genders-female" class="ml-2 text-sm text-gray-600">Female</label>
+              </div>
+
+              </div>
+              <p v-if="GenderInput" class="text-red-500 mt-2">
+                Please choose your gender!
               </p>
             </div>
             </div>
             <div>
               <button class="px-9 py-2.5 bg-black rounded-lg text-white mt-3 hover:bg-pink-500">Sign Up</button>
             </div>
-            <ul v-for="regis in regisDetail" :key="regis.id">
-              <li class="mt-3">
-                <span class="text-red-500 font-bold text-lg ">{{ regis.username }}</span> 
-                <p class="text-gray-600"> have successfully registered! <br>
+        </div>
+      </div>
+    </form>
+            <ul>
+              <li class="mt-3" v-for="regis in regisDetail" :key="regis.id">
+                <p class="text-red-500 font-bold text-lg text-center">{{ regis.username }}</p> 
+                <p class="text-gray-600 text-center"> have successfully registered! <br>
                 If you want to edit or delete your data. Please cilck.. <br></p>
                 <div class="flex justify-center">
                 <div class="mr-3">
@@ -103,9 +126,6 @@
                 </div>
               </li>
             </ul>
-        </div>
-      </div>
-    </form>
   </div>
 </template>
 
@@ -122,7 +142,7 @@ export default {
           editId: "",
           firstname: "",
           lastname: "",
-          genders: "",
+          genders: null,
           email: "",
           username: "",
           birth: null,
@@ -132,7 +152,8 @@ export default {
           UsernameInput: false,
           BirthInput: false,
           GenderInput: false,
-          regisDetail: []
+          regisDetail: [],
+          regisItem: [],
         };
   },
   methods: {
@@ -142,10 +163,10 @@ export default {
       this.EmailInput = this.email === "" ? true : false;
       this.UsernameInput = this.username === "" ? true : false;
       this.BirthInput = this.birth === null ? true : false;
-      this.GenderInput = this.genders === "" ? true : false;
+      this.GenderInput = this.genders === null ? true : false;
 
-      if(this.firstname !== "" && this.lastname !== "" && this.email !== "" &&
-        this.username !== "" && this.birth !== null && this.genders !== "") {
+      if(!this.FirstnameInput && !this.LastnameInput && !this.EmailInput &&
+        !this.UsernameInput && !this.BirthInput && !this.GenderInput) {
           if(this.edit) {
             this.editRegis({
               id: this.editId,
@@ -172,20 +193,32 @@ export default {
         this.email = "";
         this.username = "";
         this.birth = null;
-        this.genders = "";
-    },
-    async getRegisDetail() {
+        this.genders = null;
+    }, 
+    async getRegisItem(itemId) {
       try {
-        const res = await fetch(this.url);
+        this.regisItem = [];
+        const res = await fetch(`${this.url}/${itemId}`);
         const data = await res.json();
-        return data;
+        this.regisItem.push(data);
+        if(!this.edit){
+          this.edit = true
+          this.editId = data.id
+          this.firstname = data.firstname
+          this.lastname = data.lastname
+          this.email = data.email
+          this.username = data.username
+          this.birth = data.birth
+          this.genders = data.genders
+        }
+          return this.regisItem;
       } catch(error) {
         console.log(`Could not confirm. ${error}`);
       }
     },
     async addRegis(newRegis) {
       try {
-        const res = await fetch(this.url,{
+        const res = await fetch(this.url, {
           method: "POST",
           headers: {
             "content-type": "application/json"
@@ -205,10 +238,53 @@ export default {
         console.log(`Could not confirm. ${error}`);
       }
     },
-     async created() {
+    showRegis(oldRegis) {
+      this.edit = true;
+      this.editId = oldRegis.id;
+      this.firstname = oldRegis.firstname;
+      this.lastname = oldRegis.lastname;
+      this.email = oldRegis.email;
+      this.username = oldRegis.username;
+      this.birth = oldRegis.birth;
+      this.genders = oldRegis.genders;
+    },
+    async editRegis(changeDetail) {
+      try {
+        const res = await fetch(`${this.url}/${changeDetail.id}`, {
+        method: 'PUT',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          firstname: changeDetail.firstname,
+          lastname: changeDetail.lastname,
+          email: changeDetail.email,
+          username: changeDetail.username,
+          birth: changeDetail.birth,
+          genders: changeDetail.genders
+        })
+      })
+      const data = await res.json()
+      this.regisDetail = this.regisDetail.map((regis) => 
+      regis.id === changeDetail.id ? {...regis, firstname: data.firstname, 
+      lastname: data.lastname, email: data.email, username: data.username,
+      birth: data.birth, genders: data.genders} : regis)
+      this.edit = false
+      this.editId = ""
+      this.firstname = ""
+      this.lastname = ""
+      this.email = ""
+      this.username = ""
+      this.birth = null
+      this.genders = null
+      } catch(error) {
+        console.log(`Could not confirm. ${error}`);
+      }
+    },
+  },
+  async created() {
       this.regisDetail = await this.getRegisDetail();
-    }
-  }
+  },
 };
 </script>
 
